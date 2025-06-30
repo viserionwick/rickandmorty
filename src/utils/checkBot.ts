87e86ba5157f43e987f2da4aabe0e51786276098
viserionwick <viserionwick@gmail.com>
2025-06-30 12:07:18 +0300

    remove: bot detection util.

--- src/utils/checkBot.ts
+++
@@ -1,9 +0,0 @@
-// Essentials
-import { headers } from "next/headers";
-
-export async function checkBot(): Promise<boolean> {
-  const headersList = await headers();
-  const userAgent = headersList.get("user-agent") || "";
-  const isBot = /bot|crawl|spider|slurp/i.test(userAgent);
-  return isBot;
-}
